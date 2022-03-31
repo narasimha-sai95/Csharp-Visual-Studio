0e7edb0d5f47ad52fa1db9c1c27b898b531fcbc7
NarasimhaSai <180030334cse@gmail.com>
2022-03-31 18:49:10 +0530

    3

--- ConsoleApp2/ConsoleApp2/Figure.cs
+++ ConsoleApp2/ConsoleApp2/Figure.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    public class Figure
+    public abstract class Figure
     {
         public double width, height, radius;
         public const float Pi = 3.14f;
 
-
+        public abstract double GetArea();
     }
     public class Rectangle : Figure
     {
@@ -20,12 +20,20 @@ namespace ConsoleApp2
             this.width = width;
             this.height = height;   
         }
+        public override double GetArea()
+        {
+            return width * height;
+        }
     }
     public class Circle : Figure
     {
         public Circle(double radius)
         {
             this.radius = radius;
+        }
+        public override double GetArea()
+        {
+            return Pi*radius*radius;
         }
     }
     public class Cone : Figure
@@ -34,6 +42,30 @@ namespace ConsoleApp2
         {
             this.radius=radius;
             this.height=height;
+        }
+        public override double GetArea()
+        {
+            return Pi*radius*(radius+Math.Sqrt(height*height+radius*radius));
+        }
+
+       
+    }
+   class TestFigures
+    {
+        static void Main()
+        {
+            Rectangle r = new Rectangle(12.67, 56.78);
+            Circle c = new Circle(45.67);
+            Cone co = new Cone(34.98, 12.98);
+
+            Console.WriteLine("Area of Rectangle: " +r.GetArea()+"\n");
+            Console.WriteLine("Area of Circle: " + c.GetArea()+"\n");
+            Console.WriteLine("Area of Cone: " + co.GetArea());
+
+
+
+
+
         }
     }
     
